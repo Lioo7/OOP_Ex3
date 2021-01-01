d1df846ebc4a7c00fa5d8647f790d8fc421160df
Elhai <elhaimn@gmail.com>
2021-01-01 11:41:23 +0200

    update notes and edit all_in , all_out and remove_edge

--- src/DiGraph.py
+++ src/DiGraph.py
@@ -8,68 +8,120 @@ class DiGraph(GraphInterface):
     def __init__(self):
         self.nodes = {}
         self.edges = {}
+        self.Neighbors_in = {}
+        self.Neighbors_out = {}
         self.count_edges = 0
         self.mc = 0
 
+    def v_size(self) -> int:
+        """
+        Returns the number of vertices in this graph
+        @return: The number of vertices in this graph
+        """
+        return len(self.nodes)
 
-def v_size(self) -> int:
-    return len(self.nodes)
+    def e_size(self) -> int:
+        """
+          Returns the number of edges in this graph
+          @return: The number of edges in this graph
+          """
+        return self.count_edges
 
+    def get_all_v(self) -> dict:
+        """return a dictionary of all the nodes in the Graph,
+        each node is represented using apair  (key, node_data)"""
+        return self.nodes
 
-def e_size(self) -> int:
-    return self.count_edges
+    def all_in_edges_of_node(self, id1: int) -> dict:
+        """return a dictionary of all the nodes connected to (into) node_id,
+         each node is represented using a pair (key, weight)"""
+        return self.Neighbors_in.get(id1)
 
+    def all_out_edges_of_node(self, id1: int) -> dict:
+        """return a dictionary of all the nodes connected from node_id,
+        each node is represented using a pair (key, weight)"""
+        return self.Neighbors_out.get(id1)
 
-def get_all_v(self) -> dict:
-    return self.nodes
+    def get_mc(self) -> int:
+        """
+            Returns the current version of this graph,
+            on every change in the graph state - the MC should be increased
+            @return: The current version of this graph.
+            """
+        return self.mc
 
+    def add_edge(self, id1: int, id2: int, weight: float) -> bool:
+        """
+        Adds an edge to the graph.
+        @param id1: The start node of the edge
+        @param id2: The end node of the edge
+        @param weight: The weight of the edge
+        @return: True if the edge was added successfully, False o.w.
+        Note: If the edge already exists or one of the nodes dose not exists the functions will do nothing
+        """
+        # Checks if the nodes are not equal and if the wight is valid
+        if id1 != id2 and weight > 0:
+            # Checks if the nodes exist in the nodes dictionary
+            if id1 and id2 in self.nodes.keys():
+                # Checks if the edge does not exist already
+                if id1 and id2 not in self.edges:
+                    # Creates a new edge and add it to the edge dictionary
+                    edge = EdgeData(id1, id2, weight)
+                    self.edges[id1][id2] = edge
+                    # Increments the count_edges by one
+                    self.count_edges += 1
+                    # Increments the mc by one
+                    self.mc += 1
+                    return True
 
-def all_in_edges_of_node(self, id1: int) -> dict:
-    for node in self.
+    def add_node(self, node_id: int, pos: tuple = None) -> bool:
+        """
+        Adds a node to the graph.
+        @param node_id: The node ID
+        @param pos: The position of the node
+        @return: True if the node was added successfully, False o.w.
+        Note: if the node id already exists the node will not be added
+        """
+        # Checks if the node does not exist already
+        if node_id in self.nodes.keys():
+            return False
+        else:
+            # Creates a new node and adds it to the nodes dictionary
+            self.nodes[node_id] = NodeData(node_id, pos)
+            # Creates an empty inner dictionary in edges
+            self.edges.setdefault(node_id, {})
+            # Increments the mc by one
+            self.mc += 1
+        return True
 
+    def remove_node(self, node_id: int) -> bool:
+        """
+             Removes a node from the graph.
+             @param node_id: The node ID
+             @return: True if the node was removed successfully, False o.w.
+             Note: if the node id does not exists the function will do nothing
+             """
+        if node_id in self.nodes:
+            for i in self.Neighbors_in.get(node_id):
+                del self.Neighbors_in[i][node_id]
+                self.count_edges -= 1
+            for i in self.Neighbors_out.get(node_id):
+                del self.Neighbors_out[i][node_id]
+                self.count_edges -= 1
 
-def all_out_edges_of_node(self, id1: int) -> dict:
-    pass
-
-
-def get_mc(self) -> int:
-    return self.mc
-
-
-def add_edge(self, id1: int, id2: int, weight: float) -> bool:
-    # Checks if the nodes are not equal and if the wight is valid
-    if id1 != id2 and weight > 0:
-        # Checks if the nodes exist in the nodes dictionary
-        if id1 and id2 in self.nodes.keys():
-            # Checks if the edge does not exist already
-            if id1 and id2 not in self.edges:
-                # Creates a new edge and add it to the edge dictionary
-                edge = EdgeData(id1, id2, weight)
-                self.edges[id1][id2] = edge
-                # Increments the count_edges by one
-                self.count_edges += 1
-                # Increments the mc by one
-                self.mc += 1
-                return True
-
-
-def add_node(self, node_id: int, pos: tuple = None) -> bool:
-    # Checks if the node does not exist already
-    if node_id in self.nodes.keys():
+            del self.nodes[node_id]
+            del self.Neighbors_in[node_id]
+            del self.Neighbors_out[node_id]
+            self.mc += 1
+            return True
         return False
-    else:
-        # Creates a new node and adds it to the nodes dictionary
-        self.nodes[node_id] = NodeData(node_id, pos)
-        # Creates an empty inner dictionary in edges
-        self.edges.setdefault(node_id, {})
-        # Increments the mc by one
-        self.mc += 1
-    return True
 
-
-def remove_node(self, node_id: int) -> bool:
-    pass
-
-
-def remove_edge(self, node_id1: int, node_id2: int) -> bool:
-    pass
+    def remove_edge(self, node_id1: int, node_id2: int) -> bool:
+        """
+         Removes an edge from the graph.
+         @param node_id1: The start node of the edge
+         @param node_id2: The end node of the edge
+         @return: True if the edge was removed successfully, False o.w.
+         Note: If such an edge does not exists the function will do nothing
+         """
+        pass
