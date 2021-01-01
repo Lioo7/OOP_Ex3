7c3d72e559339ddc03c5856fff7ae81bf8648d99
Lioo7 <lioz.akirav@gmail.com>
2021-01-01 02:20:59 +0200

    fix add_node func

--- src/DiGraph.py
+++ src/DiGraph.py
@@ -1,3 +1,4 @@
+from edge_data import EdgeData
 from src.GraphInterface import GraphInterface
 from src.node_data import NodeData
 
@@ -7,52 +8,67 @@ class DiGraph(GraphInterface):
     def __init__(self):
         self.nodes = {}
         self.edges = {}
-        self.nodes_Neighbors = {}
         self.count_edges = 0
         self.mc = 0
 
-    def v_size(self) -> int:
-        return len(self.nodes)
 
-    def e_size(self) -> int:
-        return self.count_edges
+def v_size(self) -> int:
+    return len(self.nodes)
 
-    def get_all_v(self) -> dict:
-        return self.nodes
 
-    def all_in_edges_of_node(self, id1: int) -> dict:
-        """
+def e_size(self) -> int:
+    return self.count_edges
 
-        :param id1:
-        :return:
-        """
-        temp = {}
-        if id1 in self.nodes:
-            return self.nodes.get(id1)
-        else:
-            return temp
 
-    def all_out_edges_of_node(self, id1: int) -> dict:
-        pass
+def get_all_v(self) -> dict:
+    return self.nodes
 
-    def get_mc(self) -> int:
-        return self.mc
 
-    def add_edge(self, id1: int, id2: int, weight: float) -> bool:
-        pass
+def all_in_edges_of_node(self, id1: int) -> dict:
+    """
 
-    def add_node(self, node_id: int, pos: tuple = None) -> bool:
-        if node_id in self.nodes.keys():
-            return False
-        else:
-            self.nodes[node_id] = NodeData(node_id, pos)
-            self.nodes_Neighbors[id] = {}
-            self.mc += 1
-            return True
+    :param id1:
+    :return:
+    """
+    temp = {}
+    if id1 in self.nodes:
+        return self.nodes.get(id1)
+    else:
+        return temp
 
-    def remove_node(self, node_id: int) -> bool:
 
-        pass
+def all_out_edges_of_node(self, id1: int) -> dict:
+    pass
 
-    def remove_edge(self, node_id1: int, node_id2: int) -> bool:
-        pass
+
+def get_mc(self) -> int:
+    return self.mc
+
+
+def add_edge(self, id1: int, id2: int, weight: float) -> bool:
+    if id1 != id2 and weight > 0:
+        if id1 and id2 in self.nodes.keys():
+            edge = EdgeData(id1, id2, weight)
+            self.edges
+
+
+def add_node(self, node_id: int, pos: tuple = None) -> bool:
+    # Checks if the node does not exist already
+    if node_id in self.nodes.keys():
+        return False
+    else:
+        # Creates a new node and adds it to the nodes dictionary
+        self.nodes[node_id] = NodeData(node_id, pos)
+        # Creates an empty inner dictionary in edges
+        self.edges.setdefault(node_id, {})
+        # Increments the mc by one
+        self.mc += 1
+    return True
+
+
+def remove_node(self, node_id: int) -> bool:
+    pass
+
+
+def remove_edge(self, node_id1: int, node_id2: int) -> bool:
+    pass
--- src/edge_data.py
+++ src/edge_data.py
@@ -1,5 +1,5 @@
 class EdgeData:
-    def __init__(self, src, dest, weight):
+    def __init__(self, src: int, dest: int, weight: float):
         self.src = src
         self.dest = dest
         self.weight = weight
